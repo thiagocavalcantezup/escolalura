2b972a6ae2c6585cb5885ec555248db52d9889b6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-06-28 14:22:28 -0300

    Visualizar dados do aluno

--- src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
+++ src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.alura.escolalura.models.Aluno;
@@ -37,6 +38,12 @@ public class AlunoController {
         List<Aluno> alunos = alunoRepository.encontrarTodos();
         model.addAttribute("alunos", alunos);
         return "aluno/listar";
+    }
+
+    @GetMapping("/alunos/{id}")
+    public String visualizar(@PathVariable String id, Model model) {
+        model.addAttribute("aluno", alunoRepository.encontrarPorId(id));
+        return "aluno/visualizar";
     }
 
 }
