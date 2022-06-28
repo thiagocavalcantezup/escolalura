17ebee5e7afadb5b01b1793d6ed484547de9d56f
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-06-28 20:19:48 -0300

    Colocando marcadores no mapa

--- src/main/resources/static/js/locatorplus.js
+++ src/main/resources/static/js/locatorplus.js
@@ -36,15 +36,6 @@ function LocatorPlus(configuration) {
   // Initialize the map -------------------------------------------------------
   locator.map = new google.maps.Map(mapEl, configuration.mapOptions);
 
-  // Store selection.
-  const selectResultItem = function (locationIdx, panToMarker, scrollToResult) {
-    locator.selectedLocationIdx = locationIdx;
-
-    if (panToMarker && (locationIdx != null)) {
-      locator.map.panTo(locator.locations[locationIdx].coords);
-    }
-  };
-
   // Create a marker for each location.
   const markers = locator.locations.map(function (location, index) {
     const marker = new google.maps.Marker({
@@ -69,78 +60,8 @@ function LocatorPlus(configuration) {
     }
     locator.map.fitBounds(bounds);
   };
+
   if (locator.locations.length) {
     locator.updateBounds();
   }
-
-  // Get the distance of a store location to the user's location,
-  // used in sorting the list.
-  const getLocationDistance = function (location) {
-    if (!locator.searchLocation) return null;
-
-    // Fall back to straight-line distance.
-    return google.maps.geometry.spherical.computeDistanceBetween(
-      new google.maps.LatLng(location.coords),
-      locator.searchLocation.location);
-  };
-
-  locator.renderResultsList = function () {
-    let locations = locator.locations.slice();
-    for (let i = 0; i < locations.length; i++) {
-      locations[i].index = i;
-    }
-    if (locator.searchLocation) {
-      locations.sort(function (a, b) {
-        return getLocationDistance(a) - getLocationDistance(b);
-      });
-    }
-  };
-
-  // Optional capability initialization --------------------------------------
-  initializeSearchInput(locator);
-
-  // Initial render of results -----------------------------------------------
-  locator.renderResultsList();
-}
-
-/** When the search input capability is enabled, initialize it. */
-function initializeSearchInput(locator) {
-  const geocodeCache = new Map();
-  const geocoder = new google.maps.Geocoder();
-
-  const updateSearchLocation = function (address, location) {
-    if (locator.searchLocationMarker) {
-      locator.searchLocationMarker.setMap(null);
-    }
-    if (!location) {
-      locator.searchLocation = null;
-      return;
-    }
-    locator.searchLocation = { 'address': address, 'location': location };
-    locator.searchLocationMarker = new google.maps.Marker({
-      position: location,
-      map: locator.map,
-      title: 'My location',
-      icon: {
-        path: google.maps.SymbolPath.CIRCLE,
-        scale: 12,
-        fillColor: '#3367D6',
-        fillOpacity: 0.5,
-        strokeOpacity: 0,
-      }
-    });
-
-    // Update the locator's idea of the user's country, used for units. Use
-    // `formatted_address` instead of the more structured `address_components`
-    // to avoid an additional billed call.
-    const addressParts = address.split(' ');
-    locator.userCountry = addressParts[addressParts.length - 1];
-
-    // Update map bounds to include the new location marker.
-    locator.updateBounds();
-
-    // Update the result list so we can sort it by proximity.
-    locator.renderResultsList();
-  };
-
 }
